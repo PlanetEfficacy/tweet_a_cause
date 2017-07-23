ae01265151bba5b1977af0f96684770939bd2f2c
PlanetEfficacy <jspevack@gmail.com>
2017-07-23 08:19:47 -0600

    Initialize twitter service

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -1,22 +1,12 @@
-# class TwitterService
-#   attr_reader :user
-#   def initialize(user)
-#     @user = user
-#   end
-#
-#
-#   def fetch_tweets
-#     # client = client
-#     client = Twitter::REST::Client.new do |config|
-#       config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-#       config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-#       # config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-#       # config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-#     end
-#     debugger
-#   end
-#   private
-#   def client
-#
-#   end
-# end
+class TwitterService
+  attr_reader :user, :client
+
+
+  def initialize(user)
+    @user = user
+    @client = user.client
+  end
+
+  def fetch_tweets
+  end
+end
--- spec/services/twitter_service_spec.rb
+++ spec/services/twitter_service_spec.rb
@@ -1,21 +1,29 @@
-# require "rails_helper"
-#
-# describe TwitterService do
-#   let!(:user) { double }
-#   describe "#new" do
-#     it "is initialized with a username" do
-#       expect(described_class.new(user)).to be_instance_of TwitterService
-#     end
-#   end
-#
-#   describe "#fetch_tweets" do
-#     let!(:client) { double }
-#     it "fetches a user's tweets" do
-#       user.stub(:id) { 123456789 }
-#
-#       client.should_receive(:user_timeline).with(123456789, trim_user: true).once
-#
-#       described_class.new(user).fetch_tweets
-#     end
-#   end
-# end
+require "rails_helper"
+
+describe TwitterService do
+  let!(:user) { double }
+  let!(:client) { double }
+
+  describe "#new" do
+    it "is initialized with a user" do
+      allow(user).to receive(:client).and_return(client)
+
+      result = described_class.new(user)
+
+      expect(result).to be_instance_of TwitterService
+      expect(result.user).to eq user
+      expect(result.client).to eq client
+    end
+  end
+
+  describe "#fetch_tweets" do
+    let!(:client) { double }
+    it "fetches a user's tweets" do
+      user.stub(:id) { 123456789 }
+
+      client.should_receive(:user_timeline).with(123456789, trim_user: true).once
+
+      described_class.new(user).fetch_tweets
+    end
+  end
+end
