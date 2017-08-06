26f0d89f956647ada8c534e3a82a3f3df30ca563
PlanetEfficacy <jspevack@gmail.com>
2017-08-06 11:02:40 -0600

    Add stripe gem

--- app/services/donation_form_service.rb
+++ app/services/donation_form_service.rb
@@ -1,7 +1,17 @@
 class DonationFormService
   attr_reader :agent
-  
+
+  CHARITY_FORM_URL = 'https://secure.givewell.org/'.freeze
+
   def initialize
     @agent ||= Mechanize.new
+  end
+
+  def page
+    agent.get CHARITY_FORM_URL
+  end
+
+  def form
+    page.forms.first
   end
 end
